67c002f997e52202d299753479eb61543b7e1048
Chuck Miller <82245768+thechuckmiller@users.noreply.github.com>
2024-09-08 21:54:01 -0500

    Fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Form from "./components/Form"
+import FormState from "./components/FormState"
+import FormHook from "./components/FormHook"
 
 function App() {
 
   return (
     <>
-      <Form></Form>
+      <FormHook />
     </>
   )
 }
